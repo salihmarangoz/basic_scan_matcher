06afde96d56d503e4c7e2fd176d9195df138303a
Salih <salih285@gmail.com>
2020-12-18 17:49:30 +0100

    init

---
+++ scripts/scan_matcher.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import numpy as np
+import rospy
+import time
+from sensor_msgs.msg import LaserScan
+import tf
+
+class BeamSensorModelScanMatcher:
+    def __init__(self):
+        pass
+
+    def match(self, ref_z, z):
+        pass
+
